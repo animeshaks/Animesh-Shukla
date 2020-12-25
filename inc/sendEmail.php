10f48baf44ffe8a81d305fa787f77651837cc575
Animesh Kumar Shukla <56479786+animeshaks@users.noreply.github.com>
2020-12-25 18:40:10 +0530

    mail fix

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -50,7 +50,7 @@ if($_POST) {
       $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
 		if ($mail) { echo "OK"; }
-      else { echo "Something went wrong. Please try again."; }
+      else { echo $siteOwnersEmail; }
 		
 	} # end if - no validation error
 
