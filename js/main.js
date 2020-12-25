b49e7379193c120c6f5e48d7178f113f295abd76
Animesh Kumar Shukla <56479786+animeshaks@users.noreply.github.com>
2020-12-25 14:19:08 +0530

    Loader changes

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@
     	$("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(30).fadeOut("slow");
+        $("#preloader").delay(10).fadeOut("slow");
 
       });       
 
